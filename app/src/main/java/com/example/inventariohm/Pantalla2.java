acf959201be5dce2db3f8b33662ee29c059757f1
EloyParga <eloygarciaparga@gmail.com>
2023-05-08 10:39:54 +0200

    Activity Pantalla2
    Se crea la Activity Pantalla2 donde se implementara mas adelante el exportar PDF

---
+++ app/src/main/java/com/example/inventariohm/Pantalla2.java
@@ -0,0 +1,14 @@
+package com.example.inventariohm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2);
+    }
+}
